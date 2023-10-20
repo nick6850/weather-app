372042e9d2510ffd8e58dc2e69f994c0cc50fdeb
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-10-21 02:05:28 +0300

    Added styling and improved functionality of the weather app
    - Included CSS styles to enhance the visual appearance of the weather app interface
    - Applied fonts and background colors to improve readability and aesthetics
    - Aligned elements and adjusted spacing to ensure proper layout
    
    - Added functionality to display weather information in both Celsius and Fahrenheit units
    - Implemented a dynamic placeholder for the city input field to show the user's current location
    - Updated the background image based on the current weather conditions
    - Resolved bugs and improved error handling for better user experience

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# weather app
- This project focuses on developing API skills by creating a weather app that utilizes the WeatherAPI (http://api.weatherapi.com). The app allows users to search for locations and toggle between Fahrenheit and Celsius units for displaying weather data.
+# Weather App
+
+This project is a weather app that utilizes the WeatherAPI to provide real-time weather information for specific locations. Users can search for a location by entering a city name, and the app will display the current weather conditions, including temperature, weather condition, and location details.
+
+## Features
+
+- Search for a location by entering a city name.
+- Toggle between Fahrenheit and Celsius units for displaying temperature.
+- Display the current weather conditions, including temperature and weather condition.
+- Show location details, such as the country and city name.
+- Change the background image based on the weather condition and whether it is day or night.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## APIs Used
+
+- WeatherAPI (http://api.weatherapi.com)
+
--- script.js
+++ script.js
@@ -39,13 +39,22 @@ if (navigator.geolocation) {
   }
 
   function insertWeather(data, units) {
-    country = data.location.country
-    city = data.location.name
-    temperature = units === 'celc' ? `${data.current.temp_c} C` : `${data.current.temp_f} F` 
+    const weatherCondition = data.current.condition.text.toLowerCase();
+    const isDay = data.current.is_day;
+    const country = data.location.country
+    const city = data.location.name
+    const icon = data.current.condition.icon
+    const temperature = units === 'celc' ? `${Math.floor(data.current.temp_c)} <span id='degree'>°C</span>` : `${Math.floor(data.current.temp_f)} <span id='degree'>°F</span>` 
 
-    weatherEl.innerHTML = `Country: ${country}
-    City: ${city}
-    Temperature: ${temperature}`
+    form.elements.city.placeholder = city + '...'
+
+    weatherEl.innerHTML = `
+    <div id='temperature'><img src='${icon}'>${temperature}</div>
+    <div class='location'>${country},</div>
+    <div class='location'>${city}</div>
+    `
+
+    changeBackground(weatherCondition, isDay)
   }
 
 
@@ -58,6 +67,38 @@ if (navigator.geolocation) {
   })
 
 
+  function changeBackground(weatherCondition, isDay) {
+    const body = document.querySelector('body');
+    let imageUrl;
+  
+    if (weatherCondition.includes('rain') || weatherCondition.includes('overcast')) {
+      if (isDay) {
+        imageUrl = 'day_rain.jpg';
+      } else {
+        imageUrl = 'night_rain.jpg';
+      }
+    } else if (weatherCondition.includes('sunny') || weatherCondition.includes('clear')) {
+      if (isDay) {
+        imageUrl = 'day_sunny.jpg';
+      } else {
+        imageUrl = 'night_clear.jpg';
+      }
+    } else if (weatherCondition.includes('cloudy')) {
+      if (isDay) {
+        imageUrl = 'day_cloudy.jpg';
+      } else {
+        imageUrl = 'night_cloudy.jpg';
+      }
+    } else {
+      imageUrl = 'default.jpg';
+    }
+  
+    body.style.backgroundImage = `url(${imageUrl})`;
+  }
+  
+  
+  
+  
 
 
 
